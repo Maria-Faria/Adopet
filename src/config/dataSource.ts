0a19890e7e6dfcbbf5e87e5c70a40047e69a9763
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-08-11 15:12:41 -0300

    creating a entity with decorators

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -1,8 +1,9 @@
 import { DataSource } from "typeorm";
+import PetEntity from "../entities/PetEntity";
 
 export const AppDataSource = new DataSource({
     type: "sqlite",
     database: "./src/config/database.sqlite",
-    entities: [], //mapear cada nova entidade como uma tabela no banco
+    entities: [PetEntity], //mapear cada nova entidade como uma tabela no banco
     synchronize: true
 })
