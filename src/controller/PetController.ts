16432e637da6309bb35b609044b259e36c8c96ee
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-08-10 22:41:33 -0300

    applying CRUD

--- src/controller/PetController.ts
+++ src/controller/PetController.ts
@@ -12,4 +12,39 @@ export default class PetController {
         petsList.push(newPet);
         res.status(201).json(newPet);
     }
+
+    listPets(req: Request, res: Response) {
+        return res.status(200).json(petsList);
+    }
+
+    updatePet(req: Request, res: Response) {
+        const { id } = req.params;
+        const { adotado, especie, idade, nome } = <TipoPet>req.body;
+        const pet = petsList.find((pet) => pet.id === Number(id));
+
+        if(!pet) {
+            return res.status(404).json({error: "Pet não encontrado"});
+        }
+
+        pet.nome = nome;
+        pet.idade = idade;
+        pet.especie = especie;
+        pet.adotado = adotado
+
+        return res.status(200).json(pet);
+    }
+
+    deletePet(req: Request, res: Response) {
+        const { id } = req.params;
+        const pet = petsList.find((pet) => pet.id === Number(id));
+
+        if(!pet) {
+            return res.status(404).json({error: "Pet não encontrado"});
+        }
+
+        const index = petsList.indexOf(pet);
+        petsList.splice(index, 1);
+
+        return res.status(200).json({message: "Pet deletado com sucesso!"});
+    }
 }
--- src/routes/petRouter.ts
+++ src/routes/petRouter.ts
@@ -5,5 +5,8 @@ const router = express.Router();
 const petController = new PetController();
 
 router.post("/", petController.createPet);
+router.get("/", petController.listPets);
+router.put('/:id', petController.updatePet);
+router.delete("/:id", petController.deletePet);
 
 export default router;
